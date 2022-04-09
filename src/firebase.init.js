6433bfe6861761a9cfd3fc3874c21e253944c4ed
mosheurpau <mosheur.pau@gamil.com>
2022-04-09 20:22:45 +0600

    Update user name to firebase and done

---
+++ src/firebase.init.js
@@ -0,0 +1,18 @@
+// Import the functions you need from the SDKs you need
+import { initializeApp } from "firebase/app";
+// TODO: Add SDKs for Firebase products that you want to use
+// https://firebase.google.com/docs/web/setup#available-libraries
+
+// Your web app's Firebase configuration
+const firebaseConfig = {
+  apiKey: "AIzaSyC2FxtPVKJz4sr0AjmuIPraMjDBRd3qA30",
+  authDomain: "email-password-auth-45656.firebaseapp.com",
+  projectId: "email-password-auth-45656",
+  storageBucket: "email-password-auth-45656.appspot.com",
+  messagingSenderId: "335099516319",
+  appId: "1:335099516319:web:576f2bd19aff289d4c99aa",
+};
+
+// Initialize Firebase
+const app = initializeApp(firebaseConfig);
+export default app;
